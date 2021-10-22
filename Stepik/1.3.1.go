8bc73ec1aff80bbedabcf1f54d293559bb407f63
KalashnikovK <kirill.kalashnikow@gmail.com>
2021-10-22 05:23:02 +0300

    add first quiz

---
+++ Stepik/1.3.1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I like Go!")
+}
+
+/* Напишите программу, которая выводит "I like Go!"
+Sample Input:
+
+Sample Output:
+
+I like Go!
+*/
